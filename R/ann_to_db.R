7f0179349f640d86992f4c39b32a614bcce4b47c
julianflowers12 <julian.flowers13@gmail.com>
2022-07-17 21:34:24 +0100

    updated

--- R/ann_to_db.R
+++ R/ann_to_db.R
@@ -4,10 +4,10 @@ library(tidyverse)
 ann <- list.files(pattern = ".ann$", full.names = TRUE)
 
 
-read_delim(ann[c(1, 3:5)])
+read_delim(ann[c(3, 6, 14)])
 
-taxa_db <- map_dfr(ann[c(1, 3:5)], ~(read_delim(.x, col_names = c("index", "locations", "taxa")) |> mutate(filename = basename((.x)))))
+taxa_db <- map(ann, ~(read_delim(.x, col_names = c("index", "locations", "taxa")) |> 
+                        mutate(filename = basename((.x)))))
 
 taxa_db |>
-  select(-5) |>
   gt::gt()
