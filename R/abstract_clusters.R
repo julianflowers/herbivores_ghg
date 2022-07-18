3aed58e6947f4f291793f7937d42dd4e2b6c8b9d
julianflowers13 <julian.flowers13@gmail.com>
2022-07-18 12:50:36 +0100

    update

--- R/abstract_clusters.R
+++ R/abstract_clusters.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(myScrapers)
 library(tidytext)
-p <- setwd("~/Dropbox/My Mac (Julians-MBP-2)/Downloads")    
+p <- here::here("C:/Users/Julian/Downloads")    
 f <- list.files(p, "txt", full.names = TRUE)
-txt <- read_delim(f[8])
+f
+txt <- read_delim(f[5])
 
 abs <- txt |>
   filter(TY %in% c("DO", "AB", "T1")) |>
@@ -14,8 +15,8 @@ abs <- txt |>
   rename(title = T1, absText = AB, pmid = DO)
 
 corpus <- create_abstract_corpus(abs)
-
-clust <- create_abstract_cluster(corpus$corpus, perplexity = 20, minPts = 4)
+set.seed <- 124
+clust <- create_abstract_cluster(corpus$corpus, perplexity = 20, minPts = 3)
 clust$cluster_count
 
 labels <- create_cluster_labels(corpus$corpus, clust$clustering)
@@ -28,14 +29,18 @@ clustered_titles <- clustered_abs |>
   select(title, clus_names)
 
 clustered_titles |>
-  filter(str_detect(clus_names, "result")) |>
-  select(title)
+  #filter(str_detect(clus_names, "result")) |>
+  arrange(clus_names) |>
+  select(clus_names, everything()) |>
+  gt::gt()
 
 ggplot() +
   geom_point(data = labels$results, aes(X1, X2, colour = clus_names), show.legend = FALSE) +
   geom_point(data = labels$plot, aes(medX, medY, colour = factor(clus_names)), 
              shape = "X", size = rel(4), show.legend = FALSE) + 
-  ggrepel::geom_label_repel(data = labels$plot, aes(medX, medY, label = clus_names), size = 2.5) 
+  ggrepel::geom_label_repel(data = labels$plot, aes(medX, medY, label = clus_names), size = 2.5) +
+  #stat_ellipse(data = labels$results, aes(X1, X2, colour = clus_names, group = clus_names))
+  theme_void()
 
 
              
