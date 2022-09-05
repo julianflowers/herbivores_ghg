949319dd403f10bc62ee35664a0dab234879aeda
julianflowers12 <julian.flowers13@gmail.com>
2022-09-05 17:17:29 +0100

    updates

--- test_lat_long.R
+++ test_lat_long.R
@@ -14,11 +14,11 @@ p3 <-  "[NSEWnsew]\\d{1,2}:\\d{1,2}:\\d{1,2}?"                                  
 p4 <- "\\d{4,6}\\D?[NSns].*\\d{4,6}\\D?[EWew]"                                                    ## easting / northing
 
 
-p5 <- "(\\d{1,2})(◦|°|\\.||8?|:|\\\\)(\\s*)(\\d{1,2}|\\d{1,2}′|\\d{1,2}\\004)(.*)([NSEW])"
+p5 <- "((\\d{1,2})(◦|°|\\.||8?|:|\\\\))(\\D*)(\\d{1,2}|\\d{1,2}′|\\d{1,2}\\004)(\\D*)([NSEW])"
 
-p6 <- "(\\d{1,2})(◦|°|\\.||8?|:|\\001)"
+p6 <- "(\\d{1,2}(◦|°|\\.||8?|:|\\001))"
 
-p7 <- "(\\d{1,2})\\D*(\\d{1,2}0)\\D*(\\d{1,2}00)\\D*([NSEWnsew])"
+p7 <- "(\\d{1,2})\\D{1,4}(\\d{1,2}0)\\D{1,3}(\\d{1,2}00)\\s*([NSEWnsew])"
 
 
 
@@ -39,9 +39,60 @@ x[1:2,] |>
   DT::datatable()
   
 
-y[31,]$text |>
-  str_squish() |>
-  str_match_all(p1) |>
+pat1 <- y |>
+  mutate(text = str_squish(text),
+         matches = str_match_all(text, p1)) |>
+  select(-text) |>
+  unnest_wider("matches") 
+
+pat2 <- y |>
+  mutate(text = str_squish(text),
+         matches = str_match_all(text, p2)) |>
+  select(-text) |>
+  unnest_longer("matches") |>
+  reactable::reactable()
+
+pat3 <- y |>
+  mutate(text = str_squish(text),
+         matches = str_match_all(text, p3)) |>
+  select(-text) |>
+  unnest_longer("matches") |>
+  reactable::reactable()
+   
+pat7 <- y |>
+  mutate(text = str_squish(text),
+         matches = str_match_all(text, p7)) |>
+  select(-text) |>
+  unnest_wider("matches") 
+
+pat5 <- y |>
+  mutate(text = str_squish(text),
+         matches = str_match_all(text, p5)) |>
+  select(-text) |>
+  unnest_wider("matches") 
+
+str_match_all(y$text, p3) |>
+  tibble() |>
+  set_names("base") |>
+  unnest_longer("base") |>
+  data.frame() |>
+  View()
+
+
+
+|>
+  set_names("X1", "X2", "X3", "X4", 
+            "X5", "X6", "X7", "X8")
+  
+
+
+|>
+  DT::datatable()
+
+pat5 |> 
+  reactable::reactable()
+
+|>
   data.frame() |>
   set_names("X1") |>
   mutate(matches = str_match_all(X1, p5)) |>
